c7de17af11a3a111df68959a2180a406e43057de
rvif <ravijeeeetsharma4444@gmail.com>
2025-02-25 16:36:09 +0000

    implemented looping over dictionary

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 for i in range(10):
     print(i)
+    
+# looping over dictionary
+d = {'a': 1, 'b': 2, 'c': 3}
+for key, value in d.items():
+    print(f"key: {key}, value: {value}")
--- while.py
+++ while.py
@@ -2,3 +2,14 @@ i = 10
 while i > 0:
     print(i)
     i -= 1
+    
+    
+# implementing the same dictionary loop using while loop
+d = {'a': 1, 'b': 2, 'c': 3}
+keys = list(d.keys())
+i = 0
+while i < len(keys):
+    key = keys[i]
+    value = d[key]
+    print(f"key: {key}, value: {value}")
+    i += 1
