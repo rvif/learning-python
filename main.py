03802cd54950f45c3db4e94ef9a7e683201c13ce
rvif <ravijeeeetsharma4444@gmail.com>
2025-02-25 17:13:35 +0000

    implemented looping over set

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ for i in range(10):
 d = {'raj': 1, 'ram': 2, 'ravi': 3}
 for key, value in d.items():
     print(f"key: {key}, value: {value}")
+    
+# looping over set
+s = {1, 2, 3, 4, 5}
+for i in s: 
+    print(i)
